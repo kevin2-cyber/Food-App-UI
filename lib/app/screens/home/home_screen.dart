b0207367fa20aa451ff5fc81227303bff7fe33c6
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-20 10:52:47 +0000

    name

--- lib/app/screens/home/home_screen.dart
+++ lib/app/screens/home/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:food_app_ui/core/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          children: [
+            Text(
+              'Text Delivery to'.toUpperCase(),
+              style: Theme.of(context)
+                  .textTheme
+                  .caption!
+                  .copyWith(color: kAccentColor),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
